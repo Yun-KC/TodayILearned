d5889b4112a2db1c3b1d126c25c7dd98e0fc494f
YUN-KC <wjdrlfgg@gmail.com>
2022-03-15 14:38:23 +0900

    overloding

--- Java/A.java
+++ Java/A.java
@@ -2,12 +2,25 @@ import java.util.Arrays;
 
 
 class A {
+  static int test = 10;
+  int abc = 'b';
   public static void main(String[] args) {
-    int[] arr = {1 ,2 ,3, 4, 5};
-    int[] tmp = new int[10];
-    System.arraycopy(arr, 0, tmp, 0, arr.length);
-    arr = tmp;
-    System.out.println(Arrays.toString(arr));
-     // [1, 2, 3, 4, 5, 0, 0, 0, 0, 0]
+    int b = new B().test1();
+    
+    System.out.println(b);
+  }
+  static int add(int x, int y) {
+    test = 11;
+    return x + y;
+  }
+}
+class B {
+  static int test(int a, int b) {
+    System.out.println("여기 실행된여");
+    return 1/1;
+  }
+  char test1() {
+    test(1,2);
+    return 'a';
   }
 }
