d3349d457f9baef9429bea8f8d604d8c3fdc8d12
YUN-KC <wjdrlfgg@gmail.com>
2022-06-06 20:36:23 +0900

    dragAndDrop 테스트

---
+++ react/practice-react/src/App.js
@@ -0,0 +1,6 @@
+import DragAndDrop from './dragAndDrop/dragAndDrop';
+const App = () => {
+  return <DragAndDrop></DragAndDrop>;
+};
+
+export default App;
---
+++ react/practice-react/src/dragAndDrop/dragAndDrop.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  height: 300px;
+  width: 100%;
+  background-color: #effbfb;
+`;
+const Element = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: ${({ color }) => color};
+  line-height: 200px;
+  text-align: center;
+`;
+const mousedownHandler = (event) => {
+  console.log(event.target.);
+  console.log('마우스다운 핸들러');
+};
+
+// {name: string, color: string};
+const items = [
+  { name: '1번 박스', color: '#F5A9A9' },
+  { name: '2번 박스', color: '#D0F5A9' },
+  { name: '3번 박스', color: '#CED8F6' },
+  { name: '4번 박스', color: '#F6CED8' },
+];
+
+const DragAndDrop = () => {
+  return (
+    <Container>
+      {items.map((el, idx) => {
+        return (
+          <Element color={el.color} key={idx} onMouseDown={mousedownHandler}>
+            {el.name}
+          </Element>
+        );
+      })}
+    </Container>
+  );
+};
+
+export default DragAndDrop;
