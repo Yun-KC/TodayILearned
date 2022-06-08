958cad96365479ec366587769555e3c6cd924d44
YUN-KC <wjdrlfgg@gmail.com>
2022-06-08 14:40:06 +0900

    react-dnd 튜토리얼

--- react/practice-react/src/react-dnd/Board.js
+++ react/practice-react/src/react-dnd/Board.js
@@ -1,25 +1,35 @@
-import Square from './Square';
+import BoardSquare from './BoradSquare';
 import Knight from './Knight';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import { moveKnight, canMoveKnight } from './game';
+
 const handleSquareClick = (toX, toY) => {
   if (canMoveKnight(toX, toY)) {
     moveKnight(toX, toY);
   }
 };
-const renderSquare = (i, [knightX, knightY]) => {
+function renderPiece(x, y, [knightX, knightY]) {
+  if (x === knightX && y === knightY) return <Knight />;
+}
+const renderSquare = (i, knightPosition) => {
   const x = i % 8;
   const y = Math.floor(i / 8);
-  const black = (x + y) % 2 === 1;
-  const isKnightHere = knightX === x && knightY === y;
-  const piece = isKnightHere ? <Knight /> : null;
   return (
     <div key={i} style={{ width: '12.5%', height: '12.5%' }} onClick={() => handleSquareClick(x, y)}>
-      <Square black={black}>{piece}</Square>
+      <BoardSquare x={x} y={y}>
+        {renderPiece(x, y, knightPosition)}
+      </BoardSquare>
     </div>
   );
 };
+
 export default function Board({ knightPosition }) {
   const squares = [];
   for (let i = 0; i < 64; i++) squares.push(renderSquare(i, knightPosition));
-  return <div style={{ width: '100%', height: '100%', display: 'flex', flexWrap: 'wrap' }}>{squares}</div>;
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <div style={{ width: '100%', height: '100%', display: 'flex', flexWrap: 'wrap' }}>{squares}</div>
+    </DndProvider>
+  );
 }
--- react/practice-react/src/react-dnd/Knight.js
+++ react/practice-react/src/react-dnd/Knight.js
@@ -1,3 +1,28 @@
-export default function Knight() {
-  return <span style={{ fontSize: '100px', lineHeight: '100px' }}>♞</span>;
+import { ItemTypes } from './Constants';
+import { useDrag } from 'react-dnd';
+
+function Knight() {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: ItemTypes.KNIGHT,
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }));
+
+  return (
+    <div
+      ref={drag}
+      style={{
+        opacity: isDragging ? 0.5 : 1,
+        fontSize: '100px',
+        lineHeight: '100px',
+        fontWeight: 'bold',
+        cursor: 'move',
+      }}
+    >
+      ♞
+    </div>
+  );
 }
+
+export default Knight;
